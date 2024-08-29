f03ee74fd70b83e268c1829239851d4a54774dab
oinktech <oinktech2024@gmail.com>
2024-08-29 12:31:47 +0800

    first commit

---
+++ download/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyusyw1DuAJs-GXVqglLeRljIKNrk0KlU",
+  authDomain: "test-e722c.firebaseapp.com",
+  projectId: "test-e722c",
+  storageBucket: "test-e722c.appspot.com",
+  messagingSenderId: "747406825871",
+  appId: "1:747406825871:web:75a92e33c76ece0bff53fa"
+};;
+
